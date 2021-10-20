f64c026ab49c8602805eb1e85bf90a963e5da170
phanchieu <phanchieudev@gmail.com>
2021-10-20 22:47:24 +0700

    'updatecode'

--- furniture/front_end/src/views/news/news.vue
+++ furniture/front_end/src/views/news/news.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    
+    <div class="news">
+      <div class="container">
+      <h1>TIN TỨC</h1>
+        <b-row>
+          <div class="sidebar_news col-4">
+            <Sidebar-news/>
+          </div>
+        </b-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SidebarNews from './sidebar_news.vue'
 export default {
+  components: { SidebarNews },
 
 }
 </script>
